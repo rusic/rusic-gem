7af62b6c555627d8bfb52dc90c2f5af9ce215576
Chris Mytton <chrism@simpleweb.co.uk>
2011-07-22 12:31:17 +0100

    use a hash like a hash

--- lib/rusic/idea.rb
+++ lib/rusic/idea.rb
@@ -2,14 +2,7 @@ module Rusic
   class Idea < Hash
     def initialize(attrs = {})
       @attrs = attrs
-    end
-
-    def method_missing(method, *args)
-      @attrs[method]
-    end
-
-    def permalink
-      "/ideas/#{@attrs['id']}"
+      self['permalink'] = "/ideas/#{@attrs['id']}"
     end
   end
 end
