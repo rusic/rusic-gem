434ef21f46a6d96da9efba30696f3cfc219cc850
Paul Springett <paul@springett.me>
2014-04-06 19:19:08 +0100

    Add requires

--- lib/rusic.rb
+++ lib/rusic.rb
@@ -2,5 +2,10 @@ require 'rusic/version'
 
 module Rusic
   require 'rusic/cli'
-  require 'rusic/generators'
+  require 'rusic/deployer'
+  require 'rusic/theme_file'
+  require 'rusic/generators/theme'
+  require 'rusic/uploaders/asset'
+  require 'rusic/uploaders/editable_asset'
+  require 'rusic/uploaders/template'
 end
